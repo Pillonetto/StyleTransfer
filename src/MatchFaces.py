685f7e98c9fad736675de7315d0bc96e25b8cbef
Matte, Amanda (Stud. DACS) <a.matte@student.maastrichtuniversity.nl>
2024-02-03 16:20:13 -0300

    Funcionalidade sem mascara

--- src/MatchFaces.py
+++ src/MatchFaces.py
@@ -1,18 +1,22 @@
-"""
-Featured-Based Image Metamorphosis
-Beier 1992
-"""
+# Feature-Based Image Metamorphosis
+# Thaddeus Baier
+
 import numpy as np
 import cv2
 
-def get_alignment(input, inputLm, exampleLm):
-    h, w, c = input.shape
-    trans_coord = np.meshgrid(range(h), range(w), indexing='ij')
-    yy, xx = trans_coord[0].astype(np.float64), trans_coord[1].astype(np.float64)  # might need to switch
 
-    xsum = xx * 0.
-    ysum = yy * 0.
-    wsum = xx * 0.
+def get_alignment(example, inputLm, exampleLm):
+    height = example.shape[0]
+    width = example.shape[1]
+    trans_coord = np.meshgrid(range(height), range(width), indexing='ij')
+    yy, xx = trans_coord[0].astype(np.float64), trans_coord[1].astype(np.float64)
+
+    #DSum
+    xsum = np.zeros(xx.shape)
+    ysum = np.zeros(yy.shape)
+    #WeightSum
+    weightsum = np.zeros(xx.shape)
+    #For each line PiQi
     for i in range(len(inputLm) - 1):
         if i in {16, 21, 26, 30, 35, 47}:
             continue
@@ -26,7 +30,10 @@ def get_alignment(input, inputLm, exampleLm):
             j = 60
         else:
             j = i + 1
-        # Computes u, v
+
+        #Section 3.3 of Baier's Paper
+
+        #calculate U,V based on Pi Qi
         p_x1, p_y1 = (inputLm[i, 0], inputLm[i, 1])
         q_x1, q_y1 = (inputLm[j, 0], inputLm[j, 1])
         qp_x1 = q_x1 - p_x1
@@ -36,45 +43,59 @@ def get_alignment(input, inputLm, exampleLm):
         u = ((xx - p_x1) * qp_x1 + (yy - p_y1) * qp_y1) / qpnorm1 ** 2
         v = ((xx - p_x1) * -qp_y1 + (yy - p_y1) * qp_x1) / qpnorm1
 
-        # Computes x', y'
+        #calculate X’, Y’ based on U,V and Pi’Qi’
         p_x2, p_y2 = (exampleLm[i, 0], exampleLm[i, 1])
         q_x2, q_y2 = (exampleLm[j, 0], exampleLm[j, 1])
         qp_x2 = q_x2 - p_x2
         qp_y2 = q_y2 - p_y2
         qpnorm2 = (qp_x2 ** 2 + qp_y2 ** 2) ** 0.5
 
-        x = p_x2 + u * (q_x2 - p_x2) + (v * -qp_y2) / qpnorm2  # X'(x)
-        y = p_y2 + u * (q_y2 - p_y2) + (v * qp_x2) / qpnorm2  # X'(y)
+        x = p_x2 + u * (q_x2 - p_x2) + (v * -qp_y2) / qpnorm2
+        y = p_y2 + u * (q_y2 - p_y2) + (v * qp_x2) / qpnorm2
 
-        # Computes weights
+        #calculate displacement Di = Xi’ - Xi for this line = sqrt(dist**2)
         d1 = ((xx - q_x1) ** 2 + (yy - q_y1) ** 2) ** 0.5
         d2 = ((xx - p_x1) ** 2 + (yy - p_y1) ** 2) ** 0.5
         d = np.abs(v)
         d[u > 1] = d1[u > 1]
         d[u < 0] = d2[u < 0]
-        W = (qpnorm1 ** 1 / (10 + d)) ** 1
 
-        wsum += W
+        #weight = (length**p / (a + dist ))**b
+        #length = qpnorm1
+        # a = 5, dist = d, b = 1, p = 1
+        W = (qpnorm1 ** 1 / (5 + d)) ** 1
+
+        #weightsum += weight
+        weightsum += W
+        #DSUM += Di * weight
         xsum += W * x
         ysum += W * y
 
-    x_m = xsum / wsum
-    y_m = ysum / wsum
-    vx = xx - x_m
-    vy = yy - y_m
+    #X’= DSUM / weightsum
+    x_m = xsum / weightsum
+    #Y’= DSUM / weightsum
+    y_m = ysum / weightsum
+
+    # Compute difference between previous pixels and new set
+    vx = 2*xx - x_m
+    vy = 2*yy - y_m
+    #Clamp values
     vx[x_m < 1] = 0
-    vx[x_m > w] = 0
+    vx[x_m > width] = 0
     vy[y_m < 1] = 0
-    vy[y_m > h] = 0
+    vy[y_m > height] = 0
 
-    vx = (vx + xx).astype(int)
-    vy = (vy + yy).astype(int)
-    vx[vx >= w] = w - 1
-    vy[vy >= h] = h - 1
+    # Round to nearest integer
+    vx = vx.astype(int)
+    vy = vy.astype(int)
 
-    warp = np.ones(input.shape)
-    warp[yy.astype(int), xx.astype(int)] = input[vy, vx]
-    # from skimage.io import imsave
-    # imsave('output/transformed.jpg', warp / 255)
-    cv2.imwrite('warped.jpg', warp/255)
-    return warp, vx, vy
+    # Clamp values to image limits
+    vx[vx >= width] = width - 1
+    vy[vy >= height] = height - 1
+
+    warp = np.ones(example.shape)
+    #Output pixels map = input[mappedValues]
+    warp[yy.astype(int), xx.astype(int)] = example[vy, vx]
+
+    cv2.imwrite('warped.jpg', warp)
+    return vx, vy
--- src/TransferStyle.py
+++ src/TransferStyle.py
@@ -1,62 +1,106 @@
-import os
 import cv2 as cv
 import numpy as np
 from face_alignment import FaceAlignment, LandmarksType
 
 from MatchFaces import get_alignment
-from LaplacianPyramid import laplacian_pyramid, get_residual, get_energy
+
 
 def style_transfer():
-    input_image = 'bradley_cooper.jpg'
-    example_image = 'jim_carrey.jpg'
+    input_image = 'C:/Users/amand/Desktop/repos/StyleTransfer/src/style2.jpg'
+    example_image = 'C:/Users/amand/Desktop/repos/StyleTransfer/src/style4.jpg'
+    bGrayEx = True
 
     input = np.float32(cv.imread(input_image))
     example = np.float32(cv.imread(example_image))
 
+    if bGrayEx:
+        input = cv.cvtColor(input, cv.COLOR_RGB2GRAY)
+        example = cv.cvtColor(example, cv.COLOR_RGB2GRAY)
+
+    example = cv.resize(example, (input.shape[1], input.shape[0]))
+
     alignment = FaceAlignment(LandmarksType.TWO_D, device='cpu', flip_input=False)
-    inputLm = alignment.get_landmarks(input)[0]
-    exampleLm = alignment.get_landmarks(example)[0]
+    inputLm = alignment.get_landmarks_from_image(input)[0]
+    exampleLm = alignment.get_landmarks_from_image(example)[0]
 
-    matchingAlignments, vx, vy = get_matching_faces(input, inputLm, exampleLm)
-
+    vx, vy = get_alignment(example, exampleLm, inputLm)
     output = get_matching_energy(input, example, vx, vy)
 
     cv.imwrite('output' + '.jpg', output)
 
 
-def get_matching_faces(input, inputLm, exampleLm):
-    return get_alignment(input, inputLm, exampleLm)
+def get_matching_energy(input, style, vx, vy):
+    h = input.shape[0]
+    w = input.shape[1]
+    # Image might have less than three channels
+    try:
+        c = input.shape[2]
+    except IndexError:
+        c = 1
+    new_h, new_w, = h, w,
+    new_style, new_input = np.copy(style), np.copy(input)
+    # new_style[style_mask == 0] = 0
+    # new_input[input_mask == 0] = 0
+    n_stacks = 7
 
+    # Build a Laplacian Stack
+    laplace_style = []
+    laplace_input = []
+    for i in range(n_stacks):
+        new_h, new_w = int(new_h / 2), int(new_w / 2)
+        new_style = cv.pyrDown(new_style, np.zeros((new_h, new_w, c)))
+        new_input = cv.pyrDown(new_input, np.zeros((new_h, new_w, c)))
+        if i is 0:
+            laplace_style.append(style - cv.resize(new_style, (w, h)))
+            laplace_input.append(input - cv.resize(new_input, (w, h)))
+        else:
+            temp_style = cv.resize(pre_style, (w, h)) - cv.resize(new_style, (w, h))
+            temp_input = cv.resize(pre_input, (w, h)) - cv.resize(new_input, (w, h))
+            laplace_style.append(temp_style)
+            laplace_input.append(temp_input)
 
-def get_matching_energy(input, example, vx, vy):
-    height, width, channelCount = input.shape
+        pre_style = new_style
+        pre_input = new_input
 
-    levels = 6
+    resid_style = cv.resize(new_style, (w, h))
 
-    examplePyramid = laplacian_pyramid(example, levels)
-    inputPyramid = laplacian_pyramid(input, levels)
+    # Compute Local Energies
+    # Power maps, Malik and Perona 1990
+    energy_style = []
+    energy_input = []
+    for i in range(n_stacks):
+        new_style_ener = cv.pyrDown(laplace_style[i] ** 2, (new_h, new_w, c))
+        new_input_ener = cv.pyrDown(laplace_input[i] ** 2, (new_h, new_w, c))
 
-    resExample = get_residual(cv.resize(example, (width, height)), levels)
+        for j in range(i - 1):
+            new_style_ener = cv.pyrDown(new_style_ener, (new_h, new_w, c))
+            new_input_ener = cv.pyrDown(new_input_ener, (new_h, new_w, c))
 
-    energyExample = get_energy(examplePyramid)
-    energyInput = get_energy(inputPyramid)
+        energy_style.append(cv.resize(np.sqrt(new_style_ener), (w, h)))
+        energy_input.append(cv.resize(np.sqrt(new_input_ener), (w, h)))
 
     # Post-process warping style stacks:
-    for i in range(len(energyExample)):
-        examplePyramid[i] = examplePyramid[i][vy, vx]
-        energyExample[i] = energyExample[i][vy, vx]
+    for i in range(len(energy_style)):
+        laplace_style[i] = laplace_style[i][vy, vx]
+        energy_style[i] = energy_style[i][vy, vx]
 
     # Compute Gain Map and Transfer
-    epsDeMaquina = 0.01 ** 2
+    eps = 0.01 ** 2
     gain_max = 2.8
     gain_min = 0.005
-    output = np.zeros((height, width, channelCount))
-    for i in range(levels):
-        gain = np.sqrt(np.divide(energyExample[i], (energyInput[i] + epsDeMaquina)))
+
+    if c > 1:
+        output = np.zeros((h, w, c))
+    else:
+        output = np.zeros((h, w))
+
+    for i in range(n_stacks):
+        gain = np.sqrt(np.divide(energy_style[i], (energy_input[i] + eps)))
         gain[gain <= gain_min] = 1
         gain[gain > gain_max] = gain_max
-        output += np.multiply(energyInput[i], gain)
-    output += resExample
+        output += np.multiply(laplace_input[i], gain)
+        #cv.imwrite('output' + str(i) + '.jpg', output)
+    output += resid_style
 
     return output
 
